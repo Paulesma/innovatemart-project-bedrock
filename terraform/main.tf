b3487d74e66f0cc6c360a8139ad4c4faaf5175af
Paulesma <paulesma93@gmail.com>
2026-06-20 08:44:30 +0300

    chore: clear runner helm release tracking and freeze pristine state

--- terraform/main.tf
+++ terraform/main.tf
@@ -393,29 +393,29 @@ resource "aws_iam_role_policy_attachment" "elbc_attach" {
   role       = aws_iam_role.elbc_role.name
 }
 
-resource "helm_release" "lb_controller" {
-  name       = "aws-load-balancer-controller"
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  namespace  = "kube-system"
+#resource "helm_release" "lb_controller" {
+#name       = "aws-load-balancer-controller"
+#repository = "https://aws.github.io/eks-charts"
+#chart      = "aws-load-balancer-controller"
+#namespace  = "kube-system"
 
-  set {
-    name  = "clusterName"
-    value = aws_eks_cluster.main.name
-  }
+# set {
+#  name  = "clusterName"
+# value = aws_eks_cluster.main.name
+#}
 
-  set {
-    name  = "serviceAccount.create"
-    value = "true"
-  }
+# set {
+#  name  = "serviceAccount.create"
+# value = "true"
+# }
 
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
+#set {
+# name  = "serviceAccount.name"
+#value = "aws-load-balancer-controller"
+# }
 
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.elbc_role.arn
-  }
-}
+#set {
+# name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#value = aws_iam_role.elbc_role.arn
+#}
+#}
